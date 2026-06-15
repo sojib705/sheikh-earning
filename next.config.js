a53ae0c1bac6a3c844df676c936fa993e95a28af
sojib705 <shegoth18@gmail.com>
2026-06-15 18:12:19 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
-/** @type {import('next').Config} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // এটি আপনার কোডের কোনো ভুল বা বাগ থাকলে তা দ্রুত ধরতে সাহায্য করবে
-  
-  // যদি ভবিষ্যতে আপনি গুগল ড্রাইভ বা অন্য কোনো অনলাইন লিংক থেকে ইমেজ (যেমন: ইউজার প্রোফাইল পিকচার) শো করাতে চান
-  images: {
-    domains: ['drive.google.com', 'lh3.googleusercontent.com'], 
+  experimental: {
+    serverComponentsExternalPackages: ['googleapis'],
   },
-
-  // কোনো কারণে Vercel-এ বিল্ড দেওয়ার সময় ছোটখাটো ওয়ার্নিং বা লিন্ট এরর ইগনোর করার জন্য (বিল্ড ফাস্ট করার ট্রিক)
-  eslint: {
-    ignoreDuringBuilds: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('googleapis');
+    }
+    return config;
   },
 };
 
